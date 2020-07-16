fbe45abcfaa5feafdecc12e846895a2e3b1ca433
andrew <pixtergoood@gmail.com>
2020-07-17 01:29:14 +0500

    камеры

---
+++ Macroscop cam/CamOnMacroscop/CamOnMacroscop/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace CamOnMacroscop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axVLCPlugin21 = new AxAXVLC.AxVLCPlugin2();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axVLCPlugin21
+            // 
+            this.axVLCPlugin21.Enabled = true;
+            this.axVLCPlugin21.Location = new System.Drawing.Point(2, 1);
+            this.axVLCPlugin21.Name = "axVLCPlugin21";
+            this.axVLCPlugin21.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axVLCPlugin21.OcxState")));
+            this.axVLCPlugin21.Size = new System.Drawing.Size(621, 448);
+            this.axVLCPlugin21.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(641, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(641, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(641, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(101, 37);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(641, 142);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(101, 39);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(641, 187);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(101, 36);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(638, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.axVLCPlugin21);
+            this.Name = "Form1";
+            this.Text = "Macroscop cameras";
+            ((System.ComponentModel.ISupportInitialize)(this.axVLCPlugin21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private AxAXVLC.AxVLCPlugin2 axVLCPlugin21;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Macroscop cam/CamOnMacroscop/CamOnMacroscop/GetNameAndId.cs
@@ -0,0 +1,78 @@
+﻿using System.Xml;
+using System.Net;
+namespace CamOnMacroscop
+{
+    class GetInformationOfConnection
+    {
+        // информация об подключаемых url две части урл нужны для более удобной передачи всего урл камеры в форму
+        private string partOfUrl1 = "http://demo.macroscop.com:8080/mobile?login=root&channelid=";
+        private string partOfUrl2 = "&resolutionX=640&resolutionY=480&fps=25";
+
+        //url для парсинга id и name в Xml формате
+        private string url = "http://demo.macroscop.com:8080/configex?login=root";
+
+        //для работы с xml документом
+        private XmlDocument xDoc = new XmlDocument();
+        private XmlElement xRoot;
+
+        //массив хранит id и name
+        private int j = 0;
+        public string[,] masNameAndId = new string[2, 5];
+
+        public string GetId(int numberId)
+        {
+            j = 0;
+            xDoc.LoadXml(new WebClient().DownloadString(url)); // загружаем xml файл
+            xRoot = xDoc.DocumentElement; // получаем данные об xml элементаъ
+            foreach (XmlNode xnode in xRoot) // делаем проход по xml документу
+            {
+                if (xnode.Name == "Channels") // в теге channel нужная  нам информация, а именно id и name
+                {
+                    foreach (XmlNode childnode in xnode.ChildNodes) // проходим по потомкам
+                    {
+                        foreach (XmlNode attr in childnode.Attributes) // получаем все атрибуты 
+                        {
+                            if (attr.Name == "Id") // записываем id  (не самый лучший способ ибо id каждый раз записывает в массив 5 элементов)
+                            {
+                                masNameAndId[0, j] = attr.Value;
+                                j += 1;
+                            }
+                        }
+                    }
+                }
+
+            }
+            return masNameAndId[0, numberId];
+        }
+        public string GetName(int numberName)  // все почти тоже самое как в GetId и тоже не самый лучший алгоритм
+        {
+            j = 0;
+            xDoc.LoadXml(new WebClient().DownloadString(url));
+            xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Name == "Channels")
+                {
+                    foreach (XmlNode childnode in xnode.ChildNodes)
+                    {
+                        foreach (XmlNode attr in childnode.Attributes)
+                        {
+                            if (attr.Name == "Name")
+                            {
+                                masNameAndId[1, j] = attr.Value;
+                                j++;
+                            }
+                        }
+
+                    }
+                }
+            }
+            return masNameAndId[1, numberName];
+        }
+
+        public string GetCamUrl(string Id)  // собираем полученную url для вывода потока mpeg
+        {
+            return partOfUrl1 + Id + partOfUrl2;
+        }
+    }
+}
