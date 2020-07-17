fcc1a61b66eb919befc5ebf2d4ffdcec4ce0316d
andrew <pixtergoood@gmail.com>
2020-07-17 13:12:06 +0500

    передал класс :/

---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CamOnMacroscop
+{
+    public partial class Form1 : Form
+    {
+        private Random randomId = new Random();
+        private int id; // принимает значение(номер id камеры которую надо вывести)
+        private GetInformationOfConnection getMyParse = new GetInformationOfConnection();
+        public Form1() // инициализируем компоненты к кнопкам добавляем имена камер
+        {
+            getMyParse.ParseIndAndUrl();
+
+            InitializeComponent();
+
+            button1.Text = getMyParse.masNameAndId[1, 4];
+            button2.Text = getMyParse.GetName(1);
+            button3.Text = getMyParse.GetName(2);
+            button4.Text = getMyParse.GetName(3);
+            button5.Text = getMyParse.GetName(4);
+            
+            label1.Text = "Macroscop demo cameras";
+
+            id = axVLCPlugin21.playlist.add
+                (
+
+                getMyParse.GetCamUrl(getMyParse.GetId(randomId.Next(0, 4))), // выбираем случайную камеру и зваускаем
+                "Cam_1"
+
+                );
+            axVLCPlugin21.playlist.playItem(id);
+
+        }
+        private void button1_Click(object sender, EventArgs e) // добавляем в плэйлист наш поток по созданному урл и проигрываем его код в других кнопках аналогичен
+        {
+
+            id = axVLCPlugin21.playlist.add
+                (
+
+                getMyParse.GetCamUrl(getMyParse.GetId(0)),
+                "Cam_1"
+
+                );
+            axVLCPlugin21.playlist.playItem(id);
+            label1.Text = getMyParse.GetName(0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            id = axVLCPlugin21.playlist.add
+              (
+
+              getMyParse.GetCamUrl(getMyParse.GetId(1)),
+              "Cam_2"
+
+              );
+            axVLCPlugin21.playlist.playItem(id);
+            label1.Text = getMyParse.GetName(1);
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            id = axVLCPlugin21.playlist.add
+                  (
+
+                  getMyParse.GetCamUrl(getMyParse.GetId(2)),
+                  "Cam_3"
+
+                  );
+            axVLCPlugin21.playlist.playItem(id);
+            label1.Text = getMyParse.GetName(2);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            id = axVLCPlugin21.playlist.add
+                (
+
+                getMyParse.GetCamUrl(getMyParse.GetId(3)),
+                "Cam_4"
+
+                );
+            axVLCPlugin21.playlist.playItem(id);
+            label1.Text = getMyParse.GetName(3);
+
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            id = axVLCPlugin21.playlist.add
+                (
+
+                getMyParse.GetCamUrl(getMyParse.GetId(4)),
+                "Cam_5"
+
+                );
+            axVLCPlugin21.playlist.playItem(id);
+            label1.Text = getMyParse.GetName(4);
+
+        }
+    }
+}
---
+++ GetNameAndId.cs
@@ -0,0 +1,69 @@
+﻿using System.Xml;
+using System.Net;
+namespace CamOnMacroscop
+{
+    class GetInformationOfConnection
+    {
+        // информация об подключаемых url две части урл нужны для более удобной передачи всего урл камеры в форму
+        private string partOfUrl1 = "http://demo.macroscop.com:8080/mobile?login=root&channelid=";
+        private string partOfUrl2 = "&resolutionX=640&resolutionY=480&fps=25";
+
+        //url для парсинга id и name в Xml формате
+        private string url = "http://demo.macroscop.com:8080/configex?login=root";
+
+        //для работы с xml документом
+        private XmlDocument xDoc = new XmlDocument();
+        private XmlElement xRoot;
+
+        //массив хранит id и name
+        private int j = 0, i = 0;
+        public string[,] masNameAndId = new string[2, 5];
+    
+
+
+        public string GetId(int numberId)
+        {
+            return masNameAndId[0, numberId];
+        }
+        public string GetName(int numberName)  
+        {
+          
+            return masNameAndId[1, numberName];
+        }
+
+        public void ParseIndAndUrl() //парсим урл заполняем Id и Name
+        {
+
+            i = j = 0;
+            xDoc.LoadXml(new WebClient().DownloadString(url));
+            xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Name == "Channels")
+                {
+                    foreach (XmlNode childnode in xnode.ChildNodes)
+                    {
+                        foreach (XmlNode attr in childnode.Attributes)
+                        {
+                            if (attr.Name == "Id")
+                            {
+                                masNameAndId[0, j] = attr.Value;
+                                j++;
+                            }
+                            if (attr.Name == "Name")
+                            {
+                                masNameAndId[1, i] = attr.Value;
+                                i++;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+        public string GetCamUrl(string Id)  // собираем полученную url для вывода потока mpeg
+        {
+            return partOfUrl1 + Id + partOfUrl2;
+        }
+    }
+}
